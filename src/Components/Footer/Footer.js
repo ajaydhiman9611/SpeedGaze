f92e83418f48c20e39bd2cf784874d309fe7057e
ajaydhiman9611 <dajayhiman9611@gmail.com>
2021-11-11 13:53:27 +0530

    Footer updated

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Footer = () => {
+    return (
+        <div className="footer-copyright text-center py-3" style={{ background: "#e8e8e8" }}>© 2021 Copyright:
+            <a> SpeedGaze</a>
+        </div>
+    )
+}
